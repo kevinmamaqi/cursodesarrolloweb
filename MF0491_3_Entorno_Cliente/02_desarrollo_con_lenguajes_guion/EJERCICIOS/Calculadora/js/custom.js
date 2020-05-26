ee4bc7967a7b66bfe25b4821a32c6989978878a7
Kevin Mamaqi <kevinmamaqi@gmail.com>
2020-05-26 18:08:45 +0200

    EJERCICIOS

--- MF0491_3_Entorno_Cliente/02_desarrollo_con_lenguajes_guion/EJERCICIOS/Calculadora/js/custom.js
+++ MF0491_3_Entorno_Cliente/02_desarrollo_con_lenguajes_guion/EJERCICIOS/Calculadora/js/custom.js
@@ -1,29 +1,44 @@
 // Que necesito para que la calculadora sea funcional.
+var x = "";
+var y = "";
+var operacion;
 
-var botones;
-var operaciones;
-var ejecutarOperaciones;
-var resultado;
-
+// Boton de resultado
 var botonResultado = document.getElementById("resultado");
 botonResultado.addEventListener("click", function () {
-	console.log("Hola he pinchado en el botón resultado.");
+	if (x !== "" && y !== "" && operacion !== undefined) {
+		numeroX = parseInt(x);
+		numeroY = parseInt(y);
+		console.log(suma(numeroX, numeroY));
+	}
 });
 
+// Botones de números
 var botonesNumeros = document.getElementsByClassName("numeros");
-
 for (let i = 0; i < botonesNumeros.length; i++) {
 	const boton = botonesNumeros[i];
 	const valor = boton.innerHTML;
 	boton.addEventListener("click", function () {
-		console.log("Hola he pinchado un numero " + valor);
+		if (operacion === undefined) {
+			x = x + valor; // Pero se puede usar x += valor (shorthand).
+			console.log("x", x);
+		} else {
+			y = y + valor;
+			console.log("y", y);
+		}
 	});
 }
 
-// botonesNumeros.addEventListener("click", function () {
-// 	console.log("Hola he pinchado un numero");
-// });
-console.log(botonesNumeros);
+// Botones de operaciones
+var botonesOperaciones = document.getElementsByClassName("operaciones");
+for (let i = 0; i < botonesOperaciones.length; i++) {
+	const boton = botonesOperaciones[i];
+	const valor = boton.innerHTML;
+	boton.addEventListener("click", function () {
+		operacion = valor;
+		console.log(operacion);
+	});
+}
 
 function suma(x, y) {
 	return x + y;
