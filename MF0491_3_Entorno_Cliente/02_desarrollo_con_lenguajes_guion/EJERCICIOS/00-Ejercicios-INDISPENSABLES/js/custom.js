e7145dd9b09717bd971a10cf066c30664762a085
Kevin Mamaqi <kevinmamaqi@gmail.com>
2020-05-28 14:51:39 +0200

    28.05

---
+++ MF0491_3_Entorno_Cliente/02_desarrollo_con_lenguajes_guion/EJERCICIOS/00-Ejercicios-INDISPENSABLES/js/custom.js
@@ -0,0 +1,100 @@
+// Ejercicio 1
+// Crea una variable y asignale tu nombre.
+// Muestra la variable en la consola.
+
+// Ejercicio 2
+// Identifica cual es el error de la siguiente expresión y corrigelo.
+var precio producto = 3.45;
+
+// Ejercicio 3
+// Asigna el resultado de una suma, una resta, una multiplicación y una división
+// a distintas variables y muestralas en la consola.
+
+// Ejercicio 4
+// ¿Qué resultado sera nuevoNumero en la siguiente expresión?
+var numero = 4;
+var nuevoNumero = numero++;
+
+// Ejercicio 5
+// Crea una variable y asigna en una sola linia el resultado de primero sumar
+// 4 y 3, 5 y 8 y después multiplicarlos.
+
+// Ejercicio 6
+// Crea un botón que al pinchar escriba en la consola "Hola mundo"
+
+// Ejercicio 7
+// Crea un botón que al pinchar muestre un dialogo "Hola mundo"
+
+// Ejercicio 8
+// Escribe un condicional para comprobar si la edad de los usuarios es mayor de 18 años.
+// Muestra el resultado en la consola.
+
+// Ejercicio 9
+// Escribe una lista de operadores comparitivos y su significado en al consola, de manera ordenada.
+
+// Ejercicio 10
+// Escribe el operador que significa Y, y muestralo en la consola.
+
+// Ejercicio 11
+// Escribe un condicional que compruebe si la edad de los ususarios es Mayor de 18 y menor de 67.
+
+// Ejercicio 12
+// Escribe cada palabra de tu nombre y apellidos en un Array.
+
+// Ejercicio 13
+// Escribe cada palabra de tu nombre y apellidos en un Objeto.
+
+// Ejericio 14
+// Transforma el array en una frase que muestre tu nombre y apellidos.
+
+// Ejercicio 15
+// Transforma el objeto en una frase que muestre tu nombre y apellidos.
+
+// Ejericicio 16
+// Elimina el nombre del array.
+
+// Ejercicio 17
+// Elimina el segundo apellido del array
+
+// Ejercicio 18
+// Escribe los caracteres que faltan de la siguiente expresión
+for var i = 0 i <= 4 i++
+
+// Ejercicio 19
+// Transforma la siguiente frase a minusculas con JS.
+// Hola Me LLAMO Kevin.
+
+// Ejercicio 20
+// Extrae la palabra Hola de la frase "Hola mundo"
+
+// Ejercicio 21
+// Encuentra la posición de la letra m en la frase "Hola mundo"
+
+// Ejercicio 22
+// Devuelve la letra u de la frase "Hola mundo";
+// Asignalo a una variable y muestarlo en la consola.
+
+// Ejercicio 23
+// Cambia la palabra mundo por clase en la frase "Hola mundo"
+
+// Ejercicio 24
+// Busca el número entero más cercano a 4.83723
+
+// Ejercicio 25
+// Crea un numero aleatorio
+
+// Ejercicio 26
+// Convierte el número 13.4 a 13
+
+// Ejercicio 27
+// Convierte con la misma función la frase "33" y "33.3" a su correspondiente número entero y fraccioal.
+
+// Ejercicio 28
+// Que día de la semana era el 20 de Abril de 1982
+
+// Ejercicio 29
+// Declara una función con los parametros a, b y c.
+
+// Ejercicio 30
+// Declara una función con el parametro mensaje
+// haz que la funciona devuelva la frase "Hola mundo"
