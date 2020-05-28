8b6d628f2b1ac59aa12bf60d72c2dfa08f3b950e
Kevin Mamaqi <kevinmamaqi@gmail.com>
2020-05-28 15:49:01 +0200

    Más ejercicios

--- MF0491_3_Entorno_Cliente/02_desarrollo_con_lenguajes_guion/EJERCICIOS/00-Ejercicios-INDISPENSABLES/js/custom.js
+++ MF0491_3_Entorno_Cliente/02_desarrollo_con_lenguajes_guion/EJERCICIOS/00-Ejercicios-INDISPENSABLES/js/custom.js
@@ -1,10 +1,13 @@
 // Ejercicio 1
 // Crea una variable y asignale tu nombre.
 // Muestra la variable en la consola.
+var nombre = "Kevin";
+console.log("Ejercicio 1: ", nombre);
 
 // Ejercicio 2
 // Identifica cual es el error de la siguiente expresión y corrigelo.
-var precio producto = 3.45;
+var precioProducto = 3.45;
+console.log("Ejercicio 2: ", precioProducto);
 
 // Ejercicio 3
 // Asigna el resultado de una suma, una resta, una multiplicación y una división
@@ -13,8 +16,8 @@ var precio producto = 3.45;
 // Ejercicio 4
 // ¿Qué resultado sera nuevoNumero en la siguiente expresión?
 // Razona y escribe porque sucede.
-var numero = 4;
-var nuevoNumero = numero++;
+// var numero = 4;
+// var nuevoNumero = numero++;
 
 // Ejercicio 5
 // Crea una variable y asigna en una sola linia el resultado de primero sumar
@@ -59,7 +62,7 @@ var nuevoNumero = numero++;
 
 // Ejercicio 18
 // Escribe los caracteres que faltan de la siguiente expresión
-for var i = 0 i <= 4 i++
+// for var i = 0 i <= 4 i++
 
 // Ejercicio 19
 // Transforma la siguiente frase a minusculas con JS.
@@ -99,3 +102,47 @@ for var i = 0 i <= 4 i++
 // Ejercicio 30
 // Declara una función con el parametro mensaje
 // haz que la funciona devuelva la frase "Hola mundo"
+
+// Ejercicio 31
+// Escribe un condicional que devuelva verdadero si el usuario se llama
+// Kevin, Jorge, Maria, Pedro, Lucas, Javier
+// Falso si se llama: Joel, Marcos, Ana Maria, Jose Maria, Arturo
+
+// Ejercicio 32
+// Escribe un while do, que cuente de 10 a 0 y diga el resultado en cada iteración.
+
+// Ejercicio 33
+// Escribe una función Javascript en el HTML de un botón que diga "hola mundo" al pinchar sobre el mismo.
+
+// Ejercicio 34
+// Crea una caja de 200px por 200px en HTML
+// Identifica con JS cuando el usuario pasa por encima y muestra el
+// mensaje ESTOY ENCIMA
+
+// Ejercicio 35
+// Crea un input email y detecta cuando el usuario pincha
+// dentro del input con Javascript.
+
+// Ejericicio 36
+// Crea un formulario de contacto en HTML con nombre, email y mensaje.
+// Cuando el usuario envia el mensaje:
+//  1. Paraliza el envio.
+//  2. Muestra un mensaje de: El usuario ha enviado el mensaje.
+
+// Ejericico 37
+// Busca dos imágenes de un perro.
+// Guardalas en la carpeta /img/
+// Introduce una en el HTML y crea un botón.
+// Haz que al pinchar sobre el botón se cambie una imagen por la otra.
+
+// Ejericicio 38
+// Crea un parrafo con la clase: "parrafo-negro"
+// Crea un botón que al pinchar sobre el mismo se cambie la clase del parrafo a: "parrafo-rojo"
+
+// Ejercicio 39
+// Crea un objeto con dos propiedades: nombre y apellidos.
+// Muestra en la consola la llave (key) apellidos.
+
+// Ejercicio 40
+// Crea una frase que contenta de 0 a 999 números, separados por comas.
+// Imprimelo en la consola.
