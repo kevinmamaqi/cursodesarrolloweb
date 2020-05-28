1b6c3ca3aa8d2f2ea350e4ba16cf59d9cebebf5f
Kevin Mamaqi <kevinmamaqi@gmail.com>
2020-05-28 15:54:02 +0200

    Ejercicios dificiles

---
+++ MF0491_3_Entorno_Cliente/02_desarrollo_con_lenguajes_guion/EJERCICIOS/02-Ejercicios-Dificiles/js/custom.js
@@ -0,0 +1,9 @@
+// Ejercicio 1
+// Representa en JS si dos rectas se cruzan
+// https://es.wikipedia.org/wiki/Intersecci%C3%B3n_de_dos_rectas
+
+// Ejercicio 2
+// Representa en JS si dos rectangulos estan encima de otro
+
+// Ejercicio 3
+// Representa en JS si un punto esta dentro de un triangulo.
