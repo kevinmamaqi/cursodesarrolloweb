a1130200a49a06c649c52d0ac8ced796b7b57716
Kevin Mamaqi <kevinmamaqi@gmail.com>
2020-06-05 18:37:48 +0200

    Lista de Tareas

--- MF0491_3_Entorno_Cliente/02_desarrollo_con_lenguajes_guion/EJERCICIOS/Ejercicio-TODO-APP/js/custom.js
+++ MF0491_3_Entorno_Cliente/02_desarrollo_con_lenguajes_guion/EJERCICIOS/Ejercicio-TODO-APP/js/custom.js
@@ -1,13 +1,15 @@
 // Variables globales
 var NOMBRE_TAREA = document.getElementById("nombre-tarea");
 var ANADIR_TAREA = document.getElementById("anadir-tarea");
-var TAREAS_NUEVAS_CAJA = document.getElementById("tareas-nuevas");
+var TAREAS_NO_HECHAS_CAJA = document.getElementById("tareas-nuevas");
+var TAREAS_HECHAS_CAJA = document.getElementById("tareas-hechas");
 
 function crearNuevaTarea(NOMBRE_TAREA) {
 	if (NOMBRE_TAREA.value === "") {
 		alert("Tienes que añadir un nombre a la Tarea");
 		return;
 	}
+
 	// Crear caja de la tarea y asignarle clase
 	var nuevaTarea = document.createElement("div");
 	nuevaTarea.classList.add("tarea-nueva");
@@ -24,17 +26,27 @@ function crearNuevaTarea(NOMBRE_TAREA) {
 
 	// Crear BOTON eliminar tarea
 	var botonEliminarTarea = document.createElement("button");
-	botonEliminarTarea.innerText = "Eliminar Tarea";
+	botonEliminarTarea.innerText = "Eliminar";
 	nuevaTarea.appendChild(botonEliminarTarea);
 
-	// Añadir EVENTO a la nueva tarea
+	// Añadir EVENTO al botón de la nueva tarea
 	var botonEliminar = nuevaTarea.getElementsByTagName("button");
 	botonEliminar[0].addEventListener("click", function () {
 		this.parentElement.remove();
 	});
 
+	// Añadir EVENTO al checkbox de la nueva tarea
+	var checkboxMarcarTarea = nuevaTarea.getElementsByTagName("input");
+	checkboxMarcarTarea[0].addEventListener("change", function () {
+		if (this.checked) {
+			TAREAS_HECHAS_CAJA.appendChild(this.parentElement);
+		} else {
+			TAREAS_NO_HECHAS_CAJA.appendChild(this.parentElement);
+		}
+	});
+
 	// Añadir la tarea a la caja de Nuevas Tareas
-	TAREAS_NUEVAS_CAJA.appendChild(nuevaTarea);
+	TAREAS_NO_HECHAS_CAJA.appendChild(nuevaTarea);
 
 	// Limpio la tarea
 	NOMBRE_TAREA.value = "";
