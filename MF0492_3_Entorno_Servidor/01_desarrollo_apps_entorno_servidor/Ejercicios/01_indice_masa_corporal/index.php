031d8243ff707d522d795746be547aa4d87c08d8
Kevin Mamaqi <kevinmamaqi@gmail.com>
2020-07-01 16:38:51 +0200

    "01.07.2020"

---
+++ MF0492_3_Entorno_Servidor/01_desarrollo_apps_entorno_servidor/Ejercicios/01_indice_masa_corporal/index.php
@@ -0,0 +1,6 @@
+<?php
+// Crear una clase para calcular el índice de masa corporal de un paciente.
+// Crear propiedades, publicas y privadas para desarrollar la tarea.
+// Crear métodos publicos y privados, para desarrollar la tarea.
+// Crear un método que devuelva el resultado en función del calculo realizado.
+
