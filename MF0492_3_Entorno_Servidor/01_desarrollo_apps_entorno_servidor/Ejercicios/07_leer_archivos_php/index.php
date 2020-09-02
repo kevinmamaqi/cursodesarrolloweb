9ff539b23f91372d85e8a0e57c91a5e395faebad
Kevin Mamaqi <kevinmamaqi@gmail.com>
2020-09-02 15:17:22 +0200

    02.09.2020

--- MF0492_3_Entorno_Servidor/01_desarrollo_apps_entorno_servidor/Ejercicios/07_leer_archivos_php/index.php
+++ MF0492_3_Entorno_Servidor/01_desarrollo_apps_entorno_servidor/Ejercicios/07_leer_archivos_php/index.php
@@ -7,6 +7,7 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
     <script src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>
+    
     <title>Leer archivos PHP</title>
 </head>
 <body>
