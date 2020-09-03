1eaa27b4f919071019bb5cac96ae16210a7ce4c9
Kevin Mamaqi <kevinmamaqi@gmail.com>
2020-09-03 15:16:24 +0200

    03.02.2020

--- MF0492_3_Entorno_Servidor/02_acceso_datos_apps_web/DirectorioCasasPHP/templates/anadir-propiedad.php
+++ MF0492_3_Entorno_Servidor/02_acceso_datos_apps_web/DirectorioCasasPHP/templates/anadir-propiedad.php
@@ -3,8 +3,6 @@
 include "./classes/class.Validacion.php";
 $Validacion = new Validacion();
 
-
-
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
     // COMPROBAMOS SI SE HA ENVIADO PROVINCIA Y LO ENVIAMOS 
     $aire_acondicionado = !empty($_POST["aire_acondicionado"]) ? $_POST["aire_acondicionado"] : "";
@@ -24,10 +22,10 @@ $hayerrores = $Validacion->hayErrores();
 ?>
 
 <?php
-echo "<pre>";
-var_dump($Validacion->aire_acondicionado);
-var_dump($aire_acondicionado);
-echo "</pre>";
+// echo "<pre>";
+// var_dump($Validacion->aire_acondicionado);
+// var_dump($aire_acondicionado);
+// echo "</pre>";
 ?>
 <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
 
@@ -59,14 +57,62 @@ echo "</pre>";
         <?php endif; ?>
     </div>
 
+    <!-- TEXTAREA -->
     <div class="grupo">
         <label for="descripcion">Descripción</label>
-        <textarea name="descripcion" id="descripcion"></textarea>
+       
+        <!-- PRIMERO COMPROBAR QUE SE HA ENVIADO Y HAY ERROR -->
+        <?php if ($existeValidacion && !empty($Validacion->errores["titulo"])) : ?>
+
+            <!-- SEGUNDO COMPROBAR QUE ERROR ES (VACIO O ERRONEO) -->
+            <!-- MOSTRAR EL ERROR DE MANERA ACORDE -->
+            <?php if (!empty($Validacion->errores["titulo"]["vacio"])) : ?>
+             
+                <textarea class="error-input" name="descripcion" id="descripcion"><?php echo $Validacion->descripcion; ?></textarea>
+                <?php echo $Validacion->mensajes_error["descripcion"]["vacio"]; ?>
+
+            <?php else : ?>
+                <textarea class="error-input" name="descripcion" id="descripcion"><?php echo $Validacion->descripcion; ?></textarea>
+                <?php echo $Validacion->mensajes_error["descripcion"]["erroneo"]; ?>
+
+            <?php endif; ?>
+
+        <?php elseif ($existeValidacion && empty($Validacion->errores["descripcion"])) : ?>
+            <textarea class="valid-input" name="descripcion" id="descripcion"><?php echo $Validacion->descripcion; ?></textarea>
+
+        <?php else : ?>
+            <textarea name="descripcion" id="descripcion"></textarea>
+        
+        <?php endif; ?>
     </div>
 
+
+    <!-- INPUT NÚMERO DE HABITACIONES -->
     <div class="grupo">
         <label for="n_habitaciones">Número de habitaciones</label>
-        <input type="number" name="n_habitaciones" id="n_habitaciones">
+       
+        <!-- PRIMERO COMPROBAR QUE SE HA ENVIADO Y HAY ERROR -->
+        <?php if ($existeValidacion && !empty($Validacion->errores["n_habitaciones"])) : ?>
+
+            <!-- SEGUNDO COMPROBAR QUE ERROR ES (VACIO O ERRONEO) -->
+            <!-- MOSTRAR EL ERROR DE MANERA ACORDE -->
+            <?php if (!empty($Validacion->errores["n_habitaciones"]["vacio"])) : ?>
+                <input class="error-input" type="number" name="n_habitaciones" id="n_habitaciones" placeholder="Número de habitaciones">
+                <?php echo $Validacion->mensajes_error["n_habitaciones"]["vacio"]; ?>
+
+            <?php else : ?>
+                <input class="error-input" type="number" name="n_habitaciones" id="n_habitaciones" placeholder="Número de habitaciones" value="<?php echo $Validacion->n_habitaciones; ?>">
+                <?php echo $Validacion->mensajes_error["n_habitaciones"]["erroneo"]; ?>
+
+            <?php endif; ?>
+
+        <?php elseif ($existeValidacion && empty($Validacion->errores["n_habitaciones"])) : ?>
+            <input class="valid-input" type="number" name="n_habitaciones" id="n_habitaciones" placeholder="Número de habitaciones" value="<?php echo $Validacion->n_habitaciones; ?>">
+
+        <?php else : ?>
+            <input type="number" name="n_habitaciones" id="n_habitaciones" placeholder="Número de habitaciones">
+        
+        <?php endif; ?>
     </div>
 
     <div class="grupo grupo-checkbox">
